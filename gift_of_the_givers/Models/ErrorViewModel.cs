333d540af5b821c49b29873c7ec5cc7164fb1f11
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-09-25 12:07:15 +0200

    Revert "Add project files."
    This reverts commit 68dab13e019e44da97399710c253cc9194ae6b0d.

--- gift_of_the_givers/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace gift_of_the_givers.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
